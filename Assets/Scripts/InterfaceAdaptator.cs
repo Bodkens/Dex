20566035c8d943900f45bc2e7437987f68f796c8
Bogdan Reznikov <bogdanreznikov04@gmail.com>
2022-08-05 01:50:41 +0200

    Added InterfaceAdaptator

---
+++ Assets/Scripts/InterfaceAdaptator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceAdaptator : MonoBehaviour
+{
+    public RectTransform coordsVertical;
+    public RectTransform coordsHorizontal;
+
+    public RectTransform objectCords;
+
+    void FixedUpdate(){
+        if (Screen.orientation == ScreenOrientation.LandscapeLeft || Screen.orientation == ScreenOrientation.LandscapeRight){
+            objectCords.position = coordsHorizontal.position;
+            objectCords.anchorMax = coordsHorizontal.anchorMax;
+            objectCords.anchorMin = coordsHorizontal.anchorMin;
+        }
+        else if (Screen.orientation == ScreenOrientation.Portrait || Screen.orientation == ScreenOrientation.PortraitUpsideDown){
+            objectCords.position = coordsVertical.position;
+            objectCords.anchorMax = coordsVertical.anchorMax;
+            objectCords.anchorMin = coordsVertical.anchorMin;
+
+    }
+}
+
+}
--- Assets/Scripts/OrientationScript.cs
+++ Assets/Scripts/OrientationScript.cs
@@ -8,39 +8,52 @@ public class OrientationScript : MonoBehaviour
     public bool allowVertical;
     public GameObject menuHorizontal;
 
+    public List<RectTransform> cordLib;
+
     public GameObject menuVertical;
 
     public GameObject logoVertical;
-    // Start is called before the first frame update
-    
+ 
 
     void Start(){
         if(!allowVertical){
             Screen.autorotateToPortrait = false;
             menuVertical.SetActive(false);
             menuHorizontal.SetActive(true);
-            logoVertical.SetActive(false);
+            if(logoVertical != null){
+                logoVertical.SetActive(false);
+            }
+            
         }
         else{
             Screen.autorotateToPortrait = true;
             menuHorizontal.SetActive(false);
             menuVertical.SetActive(true);
-            logoVertical.SetActive(true);
+            if(logoVertical != null){
+                logoVertical.SetActive(true);
+            }
+            
+
         }
     }
     void FixedUpdate(){
-        if(allowVertical){
-            if(Screen.orientation == ScreenOrientation.LandscapeLeft || Screen.orientation == ScreenOrientation.LandscapeRight){
-            menuHorizontal.SetActive(true);
-            menuVertical.SetActive(false);
-            logoVertical.SetActive(false);
-            } else if(Screen.orientation == ScreenOrientation.Portrait || Screen.orientation == ScreenOrientation.PortraitUpsideDown){
+        if (allowVertical){
+            if (Screen.orientation == ScreenOrientation.LandscapeLeft || Screen.orientation == ScreenOrientation.LandscapeRight){
+                menuHorizontal.SetActive(true);
+                menuVertical.SetActive(false);
+
+                if (logoVertical != null){
+                    logoVertical.SetActive(false);
+                }
+
+            }
+            else if (Screen.orientation == ScreenOrientation.Portrait || Screen.orientation == ScreenOrientation.PortraitUpsideDown){
                 menuHorizontal.SetActive(false);
                 menuVertical.SetActive(true);
-                logoVertical.SetActive(true);
+                if (logoVertical != null){
+                    logoVertical.SetActive(true);
+                }
             }
         }
     }
-
-    // Update is called once per frame
 }
