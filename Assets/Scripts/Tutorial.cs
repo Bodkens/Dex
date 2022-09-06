6d423d5ff786704498922207afb9737aa40e02b7
Bodkens <bogdanreznikov04@gmail.com>
2022-09-06 21:33:23 +0200

    Started development of tutorial

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : Game {
+    public Player player;
+
+    public Bot bot;
+    public override void onStart() {
+
+        Screen.autorotateToPortrait = true;
+        player.Create();
+        bot.Create();
+        playersList.Add(bot);
+        playersList.Add(player);
+        Game.gameStarted = true;
+    }
+}
